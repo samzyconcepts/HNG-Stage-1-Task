3b4d2c24dff0935fc822f87361578e7c834aa691
samzyconcepts <samzyolufemi@gmail.com>
2022-10-26 15:07:31 +0100

    creating links button

--- src/Components/Link.jsx
+++ src/Components/Link.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
+import React from 'react';
 import Header from './Header';
 
 const Link = () => {
-  return <div>
-    <Header />
-  </div>;
+  return (
+    <div>
+      <Header />
+      <div className='links'>
+        <a href='https://twitter.com/Cre8veSam' className='btn'>
+          Twitter Link
+        </a>
+        <a href='https://training.zuri.team/' className='btn' id='books'>
+          Zuri Team
+        </a>
+        <a href='https://books.zuri.team' className='btn' id='btn__zuri'>
+          Zuri Books
+        </a>
+        <a
+          href='https://books.zuri.team/python-for-beginners?ref_id=Samzy'
+          className='btn'
+          id='book__python'
+        >
+          Python Books
+        </a>
+        <a href='https://background.zuri.team' className='btn' id='pitch'>
+          Background Check for Coders
+        </a>
+        <a
+          href='https://books.zuri.team/design-rules'
+          className='btn'
+          id='book__design'
+        >
+          Design Books
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Link;
